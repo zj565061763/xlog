cbd43811c66fda1eba595656ce6bc553dcec1247
zjun <565061763@qq.com>
2023-11-29 19:53:32 +0800

    demo

--- app/src/main/java/com/sd/demo/xlog/log/AppLogExecutor.kt
+++ app/src/main/java/com/sd/demo/xlog/log/AppLogExecutor.kt
@@ -5,15 +5,8 @@ import java.util.concurrent.Executors
 
 private val logExecutor = Executors.newSingleThreadExecutor()
 
-class AppLogExecutor(private val debug: Boolean) : FLogExecutor {
-
+class AppLogExecutor : FLogExecutor {
     override fun submit(task: Runnable) {
-        if (debug) {
-            // debug模式下直接执行
-            task.run()
-        } else {
-            // release模式下异步执行
-            logExecutor.submit(task)
-        }
+        logExecutor.submit(task)
     }
 }
