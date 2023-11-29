5d718a474c3a8633734a6daaecfd4f83b91fac8c
zjun <565061763@qq.com>
2023-11-29 11:26:04 +0800

    demo

--- app/src/main/java/com/sd/demo/xlog/log/AppLogExecutor.kt
+++ app/src/main/java/com/sd/demo/xlog/log/AppLogExecutor.kt
@@ -1,11 +1,11 @@
 package com.sd.demo.xlog.log
 
 import com.sd.lib.xlog.FLogExecutor
-import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
 
+private val logExecutor = Executors.newSingleThreadExecutor()
+
 class AppLogExecutor(private val debug: Boolean) : FLogExecutor {
-    private var _executor: ExecutorService? = null
 
     override fun submit(task: Runnable) {
         if (debug) {
@@ -13,15 +13,10 @@ class AppLogExecutor(private val debug: Boolean) : FLogExecutor {
             task.run()
         } else {
             // release模式下异步执行
-            val executor = _executor ?: Executors.newSingleThreadExecutor().also {
-                _executor = it
-            }
-            executor.submit(task)
+            logExecutor.submit(task)
         }
     }
 
-    override fun close() {
-        _executor?.shutdown()
-        _executor = null
+    override fun close(closeable: AutoCloseable) {
     }
 }
