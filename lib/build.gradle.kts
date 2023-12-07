793a13fb83a5ef44b033b924ffe2b43ec9758eb0
zjun <565061763@qq.com>
2023-12-08 03:13:25 +0800

    1.0.0-alpha06

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 val libGroupId = "com.sd.lib.android"
 val libArtifactId = "xlog"
-val libVersionName = "1.0.0-alpha05"
+val libVersionName = "1.0.0-alpha06"
 
 android {
     namespace = "com.sd.lib.xlog"
