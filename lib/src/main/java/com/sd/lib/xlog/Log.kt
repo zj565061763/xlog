2e63fa534d7834b05a9b1fe5432656ea91ea10c6
zjun <565061763@qq.com>
2024-01-03 12:36:35 +0800

    调度器

--- lib/src/main/java/com/sd/lib/xlog/Log.kt
+++ lib/src/main/java/com/sd/lib/xlog/Log.kt
@@ -112,6 +112,9 @@ object FLog {
 
             if (_level != level) {
                 _level = level
+                _dispatcher.dispatch {
+                    // 发送一个空消息，等调度器空闲的时候处理空闲逻辑
+                }
             }
         }
     }
