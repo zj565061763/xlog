3b654c293b7f7c154baa743a5690032f96da2543
zjun <565061763@qq.com>
2024-03-20 12:21:16 +0800

    comment

--- lib/src/main/java/com/sd/lib/xlog/LogDispatcher.kt
+++ lib/src/main/java/com/sd/lib/xlog/LogDispatcher.kt
@@ -3,7 +3,7 @@ package com.sd.lib.xlog
 import java.util.concurrent.Executors
 
 /**
- * 日志调度器，实现类可以在任何线程上执行任务，但必须保证按顺序执行
+ * 日志调度器，实现类可以在任何线程上执行任务，但必须保证按提交顺序执行，一个执行完成再执行下一个
  */
 interface FLogDispatcher {
     fun dispatch(block: Runnable)
