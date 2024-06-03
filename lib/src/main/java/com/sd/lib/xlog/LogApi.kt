b8c8f2484d315d7ed836577c34a91b5eee19da1b
zjun <565061763@qq.com>
2024-06-03 09:43:28 +0800

    写法调整

--- lib/src/main/java/com/sd/lib/xlog/LogApi.kt
+++ lib/src/main/java/com/sd/lib/xlog/LogApi.kt
@@ -4,40 +4,43 @@ package com.sd.lib.xlog
  * 打印[FLogLevel.Verbose]日志
  */
 inline fun <reified T : FLogger> flogV(block: () -> Any) {
-    logInternal<T>(FLogLevel.Verbose, block)
+    logInternal(T::class.java, FLogLevel.Verbose, block)
 }
 
 /**
  * 打印[FLogLevel.Debug]日志
  */
 inline fun <reified T : FLogger> flogD(block: () -> Any) {
-    logInternal<T>(FLogLevel.Debug, block)
+    logInternal(T::class.java, FLogLevel.Debug, block)
 }
 
 /**
  * 打印[FLogLevel.Info]日志
  */
 inline fun <reified T : FLogger> flogI(block: () -> Any) {
-    logInternal<T>(FLogLevel.Info, block)
+    logInternal(T::class.java, FLogLevel.Info, block)
 }
 
 /**
  * 打印[FLogLevel.Warning]日志
  */
 inline fun <reified T : FLogger> flogW(block: () -> Any) {
-    logInternal<T>(FLogLevel.Warning, block)
+    logInternal(T::class.java, FLogLevel.Warning, block)
 }
 
 /**
  * 打印[FLogLevel.Error]日志
  */
 inline fun <reified T : FLogger> flogE(block: () -> Any) {
-    logInternal<T>(FLogLevel.Error, block)
+    logInternal(T::class.java, FLogLevel.Error, block)
 }
 
 @PublishedApi
-internal inline fun <reified T : FLogger> logInternal(level: FLogLevel, block: () -> Any) {
-    val clazz = T::class.java
+internal inline fun <T : FLogger> logInternal(
+    clazz: Class<T>,
+    level: FLogLevel,
+    block: () -> Any,
+) {
     with(FLog) {
         if (isLoggable(clazz, level)) {
             log(clazz, level, msg = block().toString())
