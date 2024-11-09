9123e01fe50bbea08ef20224f6271cef2a3e369e
zjun <565061763@qq.com>
2024-11-10 00:25:01 +0800

    comment

--- lib/src/main/java/com/sd/lib/xlog/Log.kt
+++ lib/src/main/java/com/sd/lib/xlog/Log.kt
@@ -146,7 +146,7 @@ object FLog {
     }
 
     /**
-     * 打印日志，如果[level]等于[FLogLevel.All]或者[FLogLevel.Off]则跑异常[IllegalArgumentException]
+     * 打印日志，如果[level]等于[FLogLevel.All]或者[FLogLevel.Off]则抛异常[IllegalArgumentException]
      */
     @JvmStatic
     fun log(clazz: Class<out FLogger>, level: FLogLevel, msg: String?) {
@@ -248,7 +248,7 @@ object FLog {
 
     /**
      * 打印控制台日志，不会写入到文件中，默认tag为[DefaultDebugTag]，
-     * 如果[level]等于[FLogLevel.All]或者[FLogLevel.Off]则跑异常[IllegalArgumentException]，
+     * 如果[level]等于[FLogLevel.All]或者[FLogLevel.Off]则抛异常[IllegalArgumentException]，
      * 注意：此方法不受[setConsoleLogEnabled]开关限制，只受日志等级限制
      */
     @JvmStatic
