d7512c1f2234451cba656279223db21dd1c14707
zjun <565061763@qq.com>
2024-11-10 00:55:36 +0800

    fDebug

--- lib/src/main/java/com/sd/lib/xlog/LogSafe.kt
+++ lib/src/main/java/com/sd/lib/xlog/LogSafe.kt
@@ -3,7 +3,7 @@ package com.sd.lib.xlog
 internal inline fun <R> libRunCatching(block: () -> R): Result<R> {
     return runCatching(block)
         .onFailure { e ->
-            fDebug { e.stackTraceToString() }
+            fDebug { "lib ${e.stackTraceToString()}" }
         }
 }
 
