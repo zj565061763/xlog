fc4bd0c08b0a4b352c737913eb3e38b4de1c6089
zjun <565061763@qq.com>
2025-09-02 22:29:33 +0800

    简化日志格式化逻辑

--- lib/src/main/java/com/sd/lib/xlog/LogFormatter.kt
+++ lib/src/main/java/com/sd/lib/xlog/LogFormatter.kt
@@ -9,39 +9,18 @@ interface FLogFormatter {
 
 internal fun defaultLogFormatter(): FLogFormatter = LogFormatterImpl()
 
-private class LogFormatterImpl : FLogFormatter, AutoCloseable {
-  private val _list = mutableListOf<String>()
-
-  /** 上一次打印日志的tag */
-  private var _lastLogTag = ""
-
+private class LogFormatterImpl : FLogFormatter {
   override fun format(record: FLogRecord): String = with(record) {
-    _list.clear()
     buildString {
       append(LogTime.create(millis).timeString)
-
-      val logTag = if (tag == _lastLogTag) "" else tag
-      if (logTag.isNotEmpty()) _list.add(logTag)
-      _lastLogTag = tag
-
-      _list.add(level.displayName())
-
-      if (!isMainThread) {
-        _list.add(threadID)
-      }
-
-      if (_list.isNotEmpty()) {
-        append(_list.joinToString(prefix = "[", postfix = "]", separator = "|"))
-      }
-
+      append("[")
+      append(level.displayName())
+      if (!isMainThread) append("|").append(threadID)
+      append("]")
       append(" ")
       append(msg)
       append("\n")
     }
-  }
-
-  override fun close() {
-    _lastLogTag = ""
   }
 }
 
