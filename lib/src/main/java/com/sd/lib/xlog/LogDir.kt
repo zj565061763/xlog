e53e81f2d6f45f678911e0b8fbb3b53cc8d2847a
zjun <565061763@qq.com>
2025-09-02 21:27:37 +0800

    style

--- lib/src/main/java/com/sd/lib/xlog/LogDir.kt
+++ lib/src/main/java/com/sd/lib/xlog/LogDir.kt
@@ -1,6 +1,5 @@
 package com.sd.lib.xlog
 
-import android.annotation.SuppressLint
 import android.app.ActivityManager
 import android.app.Application
 import android.content.Context
@@ -9,13 +8,10 @@ import android.os.Process
 import java.io.File
 
 fun Context.fLogDir(): File {
-  @SuppressLint("SdCardPath")
-  val dir = (filesDir ?: File("/data/data/${packageName}/files"))
-    .resolve("sd.lib.xlog")
-
+  val dir = applicationContext.filesDir.resolve("sd.lib.xlog")
   val process = currentProcess()
   return if (!process.isNullOrBlank() && process != packageName) {
-    dir.resolve(process)
+    dir.resolve(process.replace(":", "_"))
   } else {
     dir
   }
@@ -25,15 +21,10 @@ private fun Context.currentProcess(): String? {
   return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
     Application.getProcessName()
   } else {
-    runCatching {
-      val manager = getSystemService(Context.ACTIVITY_SERVICE) as ActivityManager
-      val processes = manager.runningAppProcesses
-      if (processes.isNullOrEmpty()) {
-        null
-      } else {
-        val pid = Process.myPid()
-        processes.find { it.pid == pid }?.processName
-      }
-    }.getOrNull()
+    val pid = Process.myPid()
+    (getSystemService(Context.ACTIVITY_SERVICE) as? ActivityManager)
+      ?.runningAppProcesses
+      ?.firstOrNull { it.pid == pid }
+      ?.processName
   }
 }
