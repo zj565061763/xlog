ee5eb2d7de4c78cf76247fa5448a3159ed06a61f
zjun <565061763@qq.com>
2025-09-02 21:08:31 +0800

    LogTime

--- lib/src/main/java/com/sd/lib/xlog/LogTime.kt
+++ lib/src/main/java/com/sd/lib/xlog/LogTime.kt
@@ -14,8 +14,8 @@ internal interface LogTime {
 
     fun create(millis: Long): LogTime {
       return with(sCalendar) {
-        timeInMillis = millis
-        LogTimeModel(
+        setTimeInMillis(millis)
+        LogTimeImpl(
           year = get(Calendar.YEAR),
           month = get(Calendar.MONTH) + 1,
           dayOfMonth = get(Calendar.DAY_OF_MONTH),
@@ -29,7 +29,7 @@ internal interface LogTime {
   }
 }
 
-private data class LogTimeModel(
+private data class LogTimeImpl(
   val year: Int,
   val month: Int,
   val dayOfMonth: Int,
@@ -38,11 +38,8 @@ private data class LogTimeModel(
   val second: Int,
   val millisecond: Int,
 ) : LogTime {
-  override val dateString: String
-    get() = "${year}${month.leadingZero()}${dayOfMonth.leadingZero()}"
-
-  override val timeString: String
-    get() = "${hourOfDay.leadingZero()}:${minute.leadingZero()}:${second.leadingZero()}.${millisecond.leadingZero(3)}"
+  override val dateString: String = "${year}${month.leadingZero()}${dayOfMonth.leadingZero()}"
+  override val timeString: String = "${hourOfDay.leadingZero()}:${minute.leadingZero()}:${second.leadingZero()}.${millisecond.leadingZero(3)}"
 }
 
 private fun Int.leadingZero(length: Int = 2): String {
