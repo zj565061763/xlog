cf1289bec2c8874afdbae380ca87c3c3aed006b6
zjun <565061763@qq.com>
2025-09-04 10:40:01 +0800

    sample

--- app/src/main/java/com/sd/demo/xlog/App.kt
+++ app/src/main/java/com/sd/demo/xlog/App.kt
@@ -37,6 +37,6 @@ class App : Application() {
      * 删除日志，参数saveDays表示要保留的日志天数，小于等于0表示删除全部日志，
      * 此处saveDays=1表示保留1天的日志，即保留当天的日志
      */
-    FLog.deleteLog(1)
+    FLog.deleteLog(saveDays = 1)
   }
 }
