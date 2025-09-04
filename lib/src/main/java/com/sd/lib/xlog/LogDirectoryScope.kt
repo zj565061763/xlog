7607370807a3d0bf19fa1226d8b09065ad9f33d5
zjun <565061763@qq.com>
2025-09-04 19:24:34 +0800

    压缩算法优化

--- lib/src/main/java/com/sd/lib/xlog/LogDirectoryScope.kt
+++ lib/src/main/java/com/sd/lib/xlog/LogDirectoryScope.kt
@@ -33,18 +33,12 @@ internal class LogDirectoryScopeImpl(
   }
 }
 
-/**
- * 把当前[File]列表压缩为[target]
- * @return true-成功；false-失败
- */
-private fun Array<File>?.fZipTo(target: File?): Boolean {
+private fun Array<File>?.fZipTo(target: File): Boolean {
   try {
     if (this.isNullOrEmpty()) return false
-    if (target == null) return false
     if (!target.fCreateNewFile()) return false
-    ZipOutputStream(target.outputStream()).use { outputStream ->
+    ZipOutputStream(target.outputStream().buffered()).use { outputStream ->
       for (item in this) {
-        if (!item.exists()) continue
         compressFile(
           file = item,
           filename = item.name,
@@ -67,13 +61,13 @@ private fun compressFile(
   when {
     file.isFile -> {
       outputStream.putNextEntry(ZipEntry(filename))
-      file.inputStream().use { inputStream ->
-        inputStream.copyTo(outputStream)
-      }
+      file.inputStream().use { inputStream -> inputStream.copyTo(outputStream) }
+      outputStream.closeEntry()
     }
 
     file.isDirectory -> {
-      outputStream.putNextEntry(ZipEntry(filename + File.separator))
+      outputStream.putNextEntry(ZipEntry("${filename}/"))
+      outputStream.closeEntry()
       file.listFiles()?.forEach { item ->
         compressFile(
           file = item,
@@ -85,12 +79,9 @@ private fun compressFile(
   }
 }
 
-private fun File?.fCreateNewFile(): Boolean {
+private fun File.fCreateNewFile(): Boolean {
   try {
-    this?.deleteRecursively()
-    if (this == null) return false
-    if (this.isFile) return true
-    if (this.isDirectory) this.deleteRecursively()
+    this.deleteRecursively()
     this.parentFile?.mkdirs()
     return this.createNewFile()
   } catch (e: IOException) {
