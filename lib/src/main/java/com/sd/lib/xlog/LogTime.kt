c4096eaaa1b80c5e22c6d2420ea5d1d113766bef
zjun <565061763@qq.com>
2026-08-01 02:40:50 +0800

    注释

--- lib/src/main/java/com/sd/lib/xlog/LogTime.kt
+++ lib/src/main/java/com/sd/lib/xlog/LogTime.kt
@@ -3,6 +3,13 @@ package com.sd.lib.xlog
 import java.util.Calendar
 
 internal object LogTime {
+  /**
+   * 时区在这里创建的时候就确定了，运行期间系统时区变化（跨时区、手动改设置）不会反映到日志上，
+   * 进程重启之后恢复正常，这是有意不处理的：这种情况低频，而且[FLog.deleteLog]比较用的today
+   * 和日志文件的日期都从这里取，用的是同一个时区，保留策略的判断不会错乱。
+   *
+   * 注意夏令时不受影响，[java.util.TimeZone]会按传入的时间戳动态计算偏移量
+   */
   private val _calendar = Calendar.getInstance()
 
   /** yyyyMMdd，例如：20231125 */
